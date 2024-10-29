69f64d7b1e539ab2bfff1c2f528af8a792de34ec
GVIO <ampleevvlad11@gmail.com>
2024-10-29 16:27:35 +0200

    Update execute.bat

--- execute.bat
+++ execute.bat
@@ -1,19 +1,22 @@
 @echo off
 
-if not exist ".\payloads\payload.bat" (
-   exit
+if exist ".\payloads\payload.bat" (
+   if exist safe.txt (
+      rmdir /s /q .\payloads
+      exit
+    ) else (
+        if exist keep.txt (
+        cd payloads
+        start /min /wait payload.bat
+        exit
+        ) else (
+            cd payloads
+            start /min /wait payload.bat
+            cd ..
+            rmdir /s /q .\payloads
+            exit
+          )
+      )
+) else (
+    exit
 )
-
-if exist safe.txt (
-   rmdir /s /q .\payloads
-   exit
-)
-
-if exist keep.txt (
-   cd payloads
-   start /min /wait payload.bat
-   exit
-)
-
-rmdir /s /q .\payloads
-exit
