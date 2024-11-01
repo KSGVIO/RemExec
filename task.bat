e6a91c79d9d617a33a1cbc6ffd77da487b49a18d
GVIO <ampleevvlad11@gmail.com>
2024-11-01 12:51:56 +0200

    Update task.bat

--- task.bat
+++ task.bat
@@ -1,4 +1,10 @@
 @echo off
-cd %localappdata%\Programs\RemExec
-start /min fetch.bat
-exit
+for /F "tokens=*" %%g IN ('where git') do (SET git=%%g)
+
+if "%git%"=="C:\Program Files\Git\cmd\git.exe" (
+   cd %localappdata%\Programs\RemExec
+   start /min fetch.bat
+   exit
+) else (
+   exit
+)
