9bba79e9533218b1ba57a7c645b8a9fefb2e5015
GVIO <ampleevvlad11@gmail.com>
2024-11-03 13:05:41 +0200

    Update remote.bat

--- remote.bat
+++ remote.bat
@@ -46,10 +46,15 @@ if "%1"=="--execute" (
 REM Update
 
 if "%1"=="--update" (
+   if "%1"=="auto" (
+   call "%localappdata%\Programs\RemExec\updateComponents\getUpdate.bat" > null 2>&1
+   call "%localappdata%\Programs\RemExec\updateComponents\update.bat"
+   ) else (
    echo This will download RemExec and install it! [~3 MB]
    echo ==================================================
    pause
    echo Downloading Update...
    call "%localappdata%\Programs\RemExec\updateComponents\getUpdate.bat" > null 2>&1
    call "%localappdata%\Programs\RemExec\updateComponents\update.bat"
+   )
 )
