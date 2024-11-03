2cc3a28f506289d0174c193c9ad4fefc433b846e
GVIO <ampleevvlad11@gmail.com>
2024-11-03 13:27:46 +0200

    Update update.bat

--- updateComponents/update.bat
+++ updateComponents/update.bat
@@ -1,12 +1,21 @@
 @echo off
-cd %localappdata%\Programs\RemExec\updateComponents
+cd "%localappdata%\Programs\RemExec\updateComponents"
+
+if not exist "..\config\version.txt" (
+   echo Version file not found: ..\config\version.txt
+   exit /b 1
+)
+
+if not exist "RemExec\config\version.txt" (
+   echo Version file not found: RemExec\config\version.txt
+   exit /b 1
+)
 
 for /f "delims=" %%a in ('type ..\config\version.txt') do set installed=%%a
 for /f "delims=" %%a in ('type RemExec\config\version.txt') do set carrying=%%a
 
 echo Installed version: "%installed%"
 echo Carrying version: "%carrying%"
-
 
 if "%installed%" GEQ "%carrying%" (
    echo %installed%  %carrying% a better/same version is installed
@@ -15,14 +24,11 @@ if "%installed%" GEQ "%carrying%" (
 ) else (
    xcopy "RemExec\*" "..\." /s /e /y > nul
    rmdir /s /q "RemExec"
-   cd %localappdata%\Programs\RemExec
-   del /q elevate.bat
-   del /q README.md
-   del /q .gitattributes
+   cd "%localappdata%\Programs\RemExec"
+   del /q "elevate.bat"
+   del /q "README.md"
+   del /q ".gitattributes"
    echo Update installed!
    echo -Configured version was updated from %installed% to %carrying%
    echo %carrying% > config\version.txt
-)
-if "%1"=="-exit" (
-   exit
 )
