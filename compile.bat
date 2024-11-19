1465e6e7caed748f6c2951b1ce753b6e7a4f1c53
GVIO <ampleevvlad11@gmail.com>
2024-11-19 19:08:36 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,18 +1,23 @@
 @echo off
-REM check Compilers
-if exist "C:\Cargo\compile.bat" (
-   %localappdata%\Programs\RemExec\nocargo.vbs
-   exit
-) else if exist "C:\SLN\compile.bat"(
-   %localappdata%\Programs\RemExec\nosln.vbs
-   exit
+
+REM Check for Compilers
+if not exist "C:\Cargo\compile.bat" (
+   call "%localappdata%\Programs\RemExec\nocargo.vbs"
+   exit /b
+) else if not exist "C:\SLN\compile.bat" (
+   call "%localappdata%\Programs\RemExec\nosln.vbs"
+   exit /b
 )
+
+REM Check arguments for --useCargo or --useSLN
 if "%2"=="--useCargo" (
    shift
    shift
-   C:\Cargo\compile.bat %*
-) else if "%2"=="--useSLN"(
+   call "C:\Cargo\compile.bat" %*
+) else if "%2"=="--useSLN" (
    shift
    shift
-   C:\SLN\compile.bat %*
+   call "C:\SLN\compile.bat" %*
+) else (
+   exit /b
 )
